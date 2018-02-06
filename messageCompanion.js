209f4000583e8049105fc8246c059e7c0704e419
SMASH3D <tommy.deniau@gmail.com>
2018-02-06 01:57:44 +0100

    #10 - expedition journal

--- messageCompanion.js
+++ messageCompanion.js
@@ -1,21 +1,79 @@
 var messageCompanion = function() {
-    var link = $('.insideLeftMenu_text > table > tbody > tr > td').first();
-    var elt = $('.insideLeftMenu_top');
-    var btn = $('<input type="button" value="Raid hints" class="add_button ui-button ui-widget ui-state-default ui-corner-all" id="raid-hint-btn" style="margin-top: 25px;"/>');
-    link.click(function () {
-        elt.append(btn);
-        elt.on("click", btn, function() {
+
+    var messageBox = $('.insideLeftMenu_top');
+    var spyReportLink = $('.insideLeftMenu_text > table > tbody > tr > td').first();
+    var spyHintBtn = buildCompanionButton('raid-hint-btn', 'spyReport', translate('Raid&nbsp;hints'), spyReportLink);
+    spyReportLink.click(function () {
+        messageBox.append(spyHintBtn);
+        messageBox.on("click", spyHintBtn, function() {
             raidHint();
         });
-        link.addClass('spyReport');
+        spyReportLink.addClass('spyReport SDC');
     });
 
+    var expReportLink = $('.insideLeftMenu_content > div > table > tbody > tr:nth-child(7) > td');
+    var expJournaltBtn = buildCompanionButton('exp-journal-btn', 'expReport', translate('Expedition&nbsp;journal'), expReportLink);
+    expReportLink.click(function () {
+        messageBox.append(expJournaltBtn);
+        messageBox.on("click", expJournaltBtn, function() {
+            expeditionJournal();
+        });
+        expReportLink.addClass('spyReport SDC');
+    });
+
+};
+
+var buildCompanionButton = function(id, SDCclass, label) {
+    var spyHintBtn = $('<input type="button" value="'+label+'" class="ui-button ui-state-default ui-corner-all hintbtn" id='+id+' style="margin-top: 25px;"/>');
     $('.insideLeftMenu_text > table > tbody > tr > td').click(function () {
-        if (!$( this ).hasClass('spyReport')) {
-            btn.remove();
+        if (!$(this).hasClass(SDCclass) ) {
+            $('#'+id).remove();
         }
     });
-}
+    return spyHintBtn;
+};
+
+var expeditionJournal = function () {
+    var timeExtractRegex = /([0-9]{2}). ([a-zA-Z]{3}) ([0-9]{4}), ([0-9]{2}):([0-9]{2}):([0-9]{2})/g;
+    var resourcesExtractRegex = /Rhénium ([0-9]+)\, Sélénium ([0-9]+)/;
+    $('.message_head').each(function(k, v) {
+        var expID = (v.id.match('message_([0-9]+)')[1]);
+        var tMatch = timeExtractRegex.exec(v.innerText);
+        if (tMatch !== null) {
+            var expTime  = new Date(tMatch[3], getMonthNumber(tMatch[2]), tMatch[1], tMatch[4], tMatch[5], tMatch[6]);
+            var msgText = $(this).next('tr')[0].innerText.replace(/\./g, "");
+            if (msgText.indexOf('Elle ramène') !== -1) {
+                var rMatch = msgText.match(resourcesExtractRegex);
+                var rhe = rMatch[1];
+                var sele = rMatch[2];
+
+                console.log(expTime.toISOString());
+                console.log(expID);
+                console.log(rhe);
+                console.log(sele);
+                console.log('##############');
+            }
+        }
+    });
+};
+
+var getMonthNumber = function(mon) {
+    var months = {
+        'jan' : '0',
+        'fev' : '1',
+        'mar' : '2',
+        'avr' : '3',
+        'mai' : '4',
+        'juin' : '5',
+        'juil' : '6',
+        'aou' : '7',
+        'sep' : '8',
+        'oct' : '9',
+        'nov' : '10',
+        'dec' : '11'
+    };
+    return months[mon.toLowerCase()];
+};
 
 var raidHint = function() {
     //####### CONFIG START ##########
