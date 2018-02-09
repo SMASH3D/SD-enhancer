6a453bb20e4094f5e60801e9ca6ecee48d7dd9fb
Tommy Deniau <tdeniau@freewheel.tv>
2018-02-09 17:15:22 +0100

    #9 - making shit actually work

--- overviewCompanion.js
+++ overviewCompanion.js
@@ -7,22 +7,10 @@ var getVoteTimer = function() {
         if (computedDate && computedDate.nextVoteTimer) {
             var nextPossibleVoteTime = new Date(computedDate.nextVoteTimer);
             var now = new Date();
-            if (nextPossibleVoteTime > now) {
-                $(this).parent().append('<p class="vote-timer">/!\ VOTE NOW /!\</p>');
+            if (nextPossibleVoteTime < now) {
+                $(this).parent().append('<p class="vote-timer">VOTE NOW !</p>');
             } else {
-                $('#tabs-links > table > tbody > tr:nth-child(6) > td:nth-child(2) > a').hide();
-                var resetBtn= $('<input type="button" value="reset" class="ui-button ui-state-default ui-corner-all " id="resetTimer"/>');
-
-                var timerBox = $('<span class="timer"></span>');
-                timerBox.append('<p class="vote-timer">Next votes: '+nextPossibleVoteTime.toLocaleTimeString()+'</p>');
-                timerBox.append(resetBtn);
-                $('#tabs-links > table > tbody > tr:nth-child(6) > td:nth-child(2)').append(timerBox);
-
-                timerBox.on("click", resetBtn, function() {
-                    saveVote();
-                    timerBox.remove();
-                    $('#tabs-links > table > tbody > tr:nth-child(6) > td:nth-child(2) > a').show();
-                });
+                displayTimerBox(nextPossibleVoteTime);
             }
         } else {
             $('#tabs-links > table > tbody > tr:nth-child(6) > td:nth-child(2)').append('<p>VOTE NOW</p>');
@@ -30,7 +18,8 @@ var getVoteTimer = function() {
     });
 
     $('#tabs-links > table > tbody > tr:nth-child(6) > td:nth-child(2) > a').on('mousedown', function(e){
-        saveVote();
+        var nextPossibleVoteTime = saveVote();
+        displayTimerBox(new Date(nextPossibleVoteTime));
     });
 }
 
@@ -38,6 +27,23 @@ var saveVote = function() {
     var now = new Date();
     var twoHoursLater = now.setHours(now.getHours()+2);
     chrome.storage.sync.set({'nextVoteTimer': twoHoursLater}, function() {
-        console.log('Vote Time saved', twoHoursLater);
+        console.log('Next possible vote time saved', now.toLocaleTimeString());
+    });
+    return twoHoursLater;
+}
+
+var displayTimerBox = function(nextPossibleVoteTime) {
+    $('#tabs-links > table > tbody > tr:nth-child(6) > td:nth-child(2) > a').hide();
+    var resetBtn= $('<input type="button" value="reset" class="ui-button ui-state-default ui-corner-all " id="resetTimer"/>');
+
+    var timerBox = $('<span class="timer"></span>');
+    timerBox.append('<p class="vote-timer">Next votes: '+nextPossibleVoteTime+'</p>');
+    timerBox.append(resetBtn);
+    $('#tabs-links > table > tbody > tr:nth-child(6) > td:nth-child(2)').append(timerBox);
+
+    timerBox.on("click", resetBtn, function() {
+        saveVote();
+        timerBox.remove();
+        $('#tabs-links > table > tbody > tr:nth-child(6) > td:nth-child(2) > a').show();
     });
 }
