afb75b3aee4c2063292ef7b6ff5dcb0b4097402a
Tommy Deniau <tdeniau@freewheel.tv>
2018-02-12 18:35:41 +0100

    #13 - simplifying shit - libs are for pussies

--- imperiumCompanion.js
+++ imperiumCompanion.js
@@ -24,20 +24,23 @@ var imperiumCompanion = function() {
 var extractTechLevels = function() {
     var headerExtractorRegexp = /Dialog\.info\(([0-9]+)\)/;
     var i = 0;
-    var tech = [];
+    var techCols = {};
+    var techLevels;
+    //Build map of colIndex => techID
     $('#tabs-3 > table > tbody > tr:nth-child(1) > th').each(function(k, v){
         i++;
         var matches = headerExtractorRegexp.exec(v.outerHTML);
-        if (matches && matches[1]) {
-            tech[i] = matches[1];
+        if (matches && matches[1] && matches[1].length) {
+            techCols[i] = matches[1];
         }
     });
-    var table =  $('#tabs-3 > table').tableToJSON(
-        {
-            allowHTML: true,
-            extractor : function(cellIndex, $cell) {
-                console.log(tech[cellIndex], $cell[0].innerText);
-            }
-        }
-    );
+
+    //Parse the tech levels and store them in format techID => techLvl
+    var techLevels = {};
+    var j = 0;
+    $('#tabs-3 > table > tbody > tr > td').each(function(k, v) {
+        j++;
+        techLevels[techCols[j]] = parseInt(v.innerText);
+    });
+    return techLevels;
 }
