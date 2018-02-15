fa841d1ffd7486fc66d9820d19052abfdb3e9d88
SMASH3D <tommy.deniau@gmail.com>
2018-02-15 19:00:48 +0100

    #17 - binding arrow keypress to galaxy navigation buttons

--- companions/galaxyCompanion.js
+++ companions/galaxyCompanion.js
@@ -1,5 +1,29 @@
 
 var galaxyCompanion = function() {
     $('#galaxy_form input[type="text"]').css('min-width', '');
+
+    $(document).keydown(function(e) {
+        switch(e.which) {
+            case 37: // left
+                $('input[name="systemLeft"]').click();
+                break;
+
+            case 38: // up
+                $('input[name="galaxyRight"]').click();
+                break;
+
+            case 39: // right
+                $('input[name="systemRight"]').click();
+                break;
+
+            case 40: // down
+                $('input[name="galaxyLeft"]').click();
+                break;
+
+            default: return; // exit this handler for other keys
+        }
+        e.preventDefault(); // prevent the default action (scroll / move caret)
+    });
+
 }
 
