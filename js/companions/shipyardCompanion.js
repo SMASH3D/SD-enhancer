57d5ade81eaf144c3116abd5dca54798cf8fd084
SMASH3D <tommy.deniau@gmail.com>
2018-05-17 23:12:37 +0200

    rising the building step amount to 10000000000000

--- js/companions/shipyardCompanion.js
+++ js/companions/shipyardCompanion.js
@@ -4,7 +4,7 @@ var shipyardCompanion = function() {
     var uri = window.location.pathname;
     var shipID = getUrlParameter('build');
     var targetAmount = getUrlParameter('amount');
-    var batchAmount = typeof targetAmount !== 'undefined' ? Math.min(targetAmount, 10000000) : 10000000;
+    var batchAmount = typeof targetAmount !== 'undefined' ? Math.min(targetAmount, 10000000000000) : 10000000000000;
     if (typeof(shipID) !== 'undefined' && batchAmount > 0 ) {
         targetAmount -= batchAmount;
 
